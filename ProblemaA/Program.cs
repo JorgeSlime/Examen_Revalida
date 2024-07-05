e38282c104574f58ec9b02f5588e450631d9e9b5
JorgeSlime <jt603691@gmail.com>
2024-07-05 13:25:56 -0400

    problema A

---
+++ ProblemaA/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(){
+        int N;
+        Console.Write("Ingrese el tamaño de la matriz (N x N): ");
+        N = int.Parse(Console.ReadLine());
+        int[,] matriz1 = new int[N, N];
+        int[,] matriz2 = new int[N, N];
+        int[,] resultado = new int[N, N];
+        Console.WriteLine("Primera matriz:");
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                matriz1[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Segunda matriz:");
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                matriz2[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                resultado[i, j] = 0;
+            }
+        }
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                for (int k = 0; k < N; k++){
+                    resultado[i, j] += matriz1[i, k] * matriz2[k, j];
+                }
+            }
+        }
+        Console.WriteLine("El resulta es: ");
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                Console.Write(resultado[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
